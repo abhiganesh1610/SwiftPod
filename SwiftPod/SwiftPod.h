d97e8198ff0a3dfa3ef72302dfae338ba53b1d89
Ganesh <ganesh@zeoner.com>
2025-01-27 12:58:11 +0530

    Initial Commit

---
+++ SwiftPod/SwiftPod.h
@@ -0,0 +1,18 @@
+//
+//  SwiftPod.h
+//  SwiftPod
+//
+//  Created by Ganesh on 27/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftPod.
+FOUNDATION_EXPORT double SwiftPodVersionNumber;
+
+//! Project version string for SwiftPod.
+FOUNDATION_EXPORT const unsigned char SwiftPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPod/PublicHeader.h>
+
+
---
+++ SwiftPodTests/SwiftPodTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPodTests.swift
+//  SwiftPodTests
+//
+//  Created by Ganesh on 27/01/25.
+//
+
+import Testing
+@testable import SwiftPod
+
+struct SwiftPodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
